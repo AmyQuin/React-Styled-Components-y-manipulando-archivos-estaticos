fcf732e451d70e65f5ad2bc18d0f8d9ff692aa5c
AmyQuin <149535311+AmyQuin@users.noreply.github.com>
2024-07-12 19:21:01 -0600

    02. Componentes y fuentes iniciales
    En esta lección, aprendiste cómo:
    
    -Crear y estilizar el componente del encabezado.
    -Estructurar la barra lateral.
    -Crear un ítem de lista que reciba un texto y una imagen.
    -Importar fuentes y establecerlas como predeterminadas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
+import Cabecera from "./components/Cabecera"
+import BarraLateral from "./components/BarraLateral"
 
 const FondoGradiente = styled.div`
 background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -8,12 +10,14 @@ min-height:100vh;
 `
 
 function App() {
-  
+
 
   return (
     <>
       <FondoGradiente>
-      <GlobalStyles/>
+        <GlobalStyles />
+        <Cabecera />
+        <BarraLateral/>
       </FondoGradiente>
     </>
   )
---
+++ src/components/Cabecera/index.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+import CampoTexto from "../CampoTexto"
+
+const HeaderEstilizado = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+    img{
+        width: 212px;
+    } //todas las imagenes que esten dentro de un header tendran los mismo estilos
+`
+
+
+
+
+const Cabecera = () => {
+    return <HeaderEstilizado>
+        <img src="img/logo.png" alt="Logo de Space App"/>
+        <CampoTexto/>
+    </HeaderEstilizado>
+}
+
+export default Cabecera
