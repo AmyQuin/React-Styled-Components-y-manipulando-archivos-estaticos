a5e176fd408ab9beb382e2e4a9dfc9f92fa602da
AmyQuin <149535311+AmyQuin@users.noreply.github.com>
2024-07-12 20:56:36 -0600

    03. Creando galería
    En esta lección, aprendiste cómo:
    
    -Crear un diseño de escritorio.
    -Estructurar el código de una galería de fotos con Flexbox.
    -Trabajar con etiquetas dinámicamente.
    -Importar fotos desde un archivo estático .json.

--- src/components/BarraLateral/index.jsx
+++ src/components/BarraLateral/index.jsx
@@ -12,12 +12,35 @@ const BarraLateral = () => {
     return(<aside>
         <nav>
             <ListaEstilizada>
-                <ItemNavegacion iconoActivo="iconos/home-activo.png" iconoInactivo="iconos/home-inactivo.png" activo>
+                <ItemNavegacion iconoActivo="iconos/home-activo.png" 
+                iconoInactivo="iconos/home-inactivo.png" activo
+                >
                     Inicio
                 </ItemNavegacion>
-                <ItemNavegacion iconoActivo="iconos/mas-vistas-activo.png" iconoInactivo="iconos/mas-vistas-inactivo.png">
-                    Más visitados
-                </ItemNavegacion>
+                <ItemNavegacion 
+                        iconoActivo="/iconos/mas-vistas-activo.png" 
+                        iconoInactivo="/iconos/mas-vistas-inactivo.png"
+                    >
+                        Más vistas
+                    </ItemNavegacion>
+                    <ItemNavegacion 
+                        iconoActivo="/iconos/me-gusta-activo.png" 
+                        iconoInactivo="/iconos/me-gusta-inactivo.png"
+                    >
+                        Más Me Gusta
+                    </ItemNavegacion>
+                    <ItemNavegacion 
+                        iconoActivo="/iconos/nuevas-activo.png" 
+                        iconoInactivo="/iconos/nuevas-inactivo.png"
+                    >
+                        Nuevas
+                    </ItemNavegacion>
+                    <ItemNavegacion 
+                        iconoActivo="/iconos/sorprendeme-activo.png" 
+                        iconoInactivo="/iconos/sorprendeme-inactivo.png"
+                    >
+                        Sorpréndeme
+                    </ItemNavegacion>
             </ListaEstilizada>
         </nav>
     </aside>)
